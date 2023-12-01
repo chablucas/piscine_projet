abd75d04598a1fcfd43b06f3342408943876109f
chablucas <115090822+chablucas@users.noreply.github.com>
2023-12-01 10:35:05 +0100

    forms.py

--- webtech_project/BlogMovies/forms.py
+++ webtech_project/BlogMovies/forms.py
@@ -1,18 +1,10 @@
-# forms.py
-
 from django import forms
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth.models import User
 
 class UserLoginForm(AuthenticationForm):
     username = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Username'}))
     password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Password'}))
-
-
-# forms.py
-
-from django import forms
-from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.models import User
 
 class UserRegistrationForm(UserCreationForm):
     email = forms.EmailField(max_length=254, help_text='Required. Enter a valid email address.')
